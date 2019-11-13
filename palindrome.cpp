f4e11d15959e51733ecc65ff8dd3c3fab9194e1a
Sanskar Vijpuria <54209147+sanskarvijpuria@users.noreply.github.com>
2019-11-13 23:06:41 +0530

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+    char str[50];
+    char *ptr;
+    cout<<"Enter the string:\n";
+    gets(str);
+    int s;
+
+    s=strlen(str);
+    ptr=str;
+    cout<<"String length is\n"<<s<<endl;
+
+    while(*ptr!='\0')
+    {
+    *ptr++;
+    }
+    cout<<"Palindrome\n";
+    for(int i=s;i>0;i--)
+        {
+            *ptr--;
+            cout<<*ptr;
+
+        }
+
+    getch();
+    return 0;
+}
+
